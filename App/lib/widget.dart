3dcac0d789ea9a99597d8ccd425e96a8f0332deb
Daav <daav@MacBook-Pro-de-Daav.local>
2023-12-08 01:02:23 +0100

    Début de l'interface et chargement des questions

--- App/lib/widget.dart
+++ App/lib/widget.dart
@@ -16,21 +16,95 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          width: 250,
+          height: 50,
+          child: Image.asset('assets/logo.png'),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
       body: Center(
-        child: FutureBuilder<List<Question>>(
-          future: questions,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator(); // Indicateur de chargement en cours
-            } else if (snapshot.hasError) {
-              return Text('Erreur de chargement'); // Affiche en cas d'erreur
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return Text('Aucune question trouvée'); // Aucune question dans la liste
-            } else {
-              return Text(affiche(snapshot.data![0])); // Affiche la première question
-            }
-          },
-        )
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: MediaQuery.of(context).size.height *
+                    0.1, // Position de la carte plus haute sur l'écran
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF71816D),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Question 1",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Text(
+                          "Ceci est la question...",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Logique pour traiter la réponse Faux
+                      },
+                      child: Image.asset(
+                        'assets/bouton_faux.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: () {
+                        // Logique pour traiter la réponse Vrai
+                      },
+                      child: Image.asset(
+                        'assets/bouton_vrai.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
